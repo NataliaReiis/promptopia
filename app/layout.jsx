c3bb36204ad8c67721c4e65797bb5c348c79f66b
NataliaReiis <nataliareis208@gmail.com>
2024-05-12 22:14:35 -0300

    feat: initial structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,28 @@
-import  '@styles/globals.css';
+import '@/styles/globals.css'; 
+
+import Nav from '@/components/Nav';
+import Provider from '@/components/Provider';
 
 export const metadata = {
-    title: "Promptopia",
-    descriptiomn : 'Discover & Share Ai Prompts'
-}
-const layout = () => {
-    return (
-        <div>Layout</div>
-    )
-}
+  title: "Promptopia",
+  descriptiomn: "Discover & Share Ai Prompts",
+};
 
-export default layout 
+const RootLayout = ( {children}) => {
+  return (
+    <html lang="pt-br">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+            <Nav />
+            {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
+import Feed from '@/components/Feed'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> Ai-Powered Prompyts
+        </span> 
+        </h1>
+        <p className="desc ext-center">Promptopia is an open-source AI prompting toll for modern world to discover, create and shore creative prompts</p>
+        
+        <Feed />
+        
+    </section>
   )
 }
 
