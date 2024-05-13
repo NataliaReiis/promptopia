9d3fc52398fc3ecd10059bea256bacceb2cf2f86
NataliaReiis <nataliareis208@gmail.com>
2024-05-12 21:19:57 -0300

    commit initial

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import  '@styles/globals.css';
+
+export const metadata = {
+    title: "Promptopia",
+    descriptiomn : 'Discover & Share Ai Prompts'
+}
+const layout = () => {
+    return (
+        <div>Layout</div>
+    )
+}
+
+export default layout 
